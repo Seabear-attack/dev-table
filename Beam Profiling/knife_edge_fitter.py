f19b5beb4407811e07d67f33bc81b9c9901e616f
mike-wahl <michael.j.wahl@stonybrook.edu>
2023-11-07 14:41:04 -0500

    Version 1 of knife edge fitter

--- Beam Profiling/knife_edge_fitter.py
+++ Beam Profiling/knife_edge_fitter.py
@@ -1,30 +1,45 @@
 import numpy as np
-from scipy.special import erf
 from findiff import FinDiff
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
-# from scipy.integrate import quad
 from scipy.stats import norm 
 
+
+'''
+Define the functional model of the knife edge measurement. Here we assume that the transverse profile is gaussian, so the knife edge should follow the integral of a gaussian (i.e. the cumulative distribution/cdf). Note that the 1/e^2 distance is equivalent to twice the standard deviation.
+'''
 def knifeedge(x_um, intensity_e_sq_radius, center, max_power, offset):
-    return max_power / 2 * (1 + erf((x_um - center) / (intensity_e_sq_radius / np.sqrt(2)))) + offset
+    return max_power * norm.cdf(x_um, loc=center, scale=intensity_e_sq_radius / 2) + offset
+
+
 if __name__ == "__main__":
-    x_um = np.linspace(-10000, 10000, 100)
+    x_um = np.linspace(0, 1500, 50)
+    dx = x_um[1] - x_um[0]
     stddev = 324
     middle = 500
-    power = norm.cdf(x_um, loc=middle, scale=stddev) 
+    power = np.power(norm.cdf(x_um, loc=middle, scale=stddev),1) 
 
-    popt, _ = curve_fit(knifeedge, x_um, power, ((max(x_um)-min(x_um))/2, np.mean(x_um),
+    # Do the fit
+    popt, _ = curve_fit(knifeedge, x_um, power, ((max(x_um)-min(x_um))/2, np.median(power),
                                                  max(power), min(power)))
-
-    plt.scatter(x_um, power, alpha=.5)
-    plt.plot(x_um, knifeedge(x_um, *popt), color='r', label=r'$\frac{1}{e^2}$ radius: ' f'{popt[0]: .0f} $\mu$m')
-    #plt.scatter(x_um, power, alpha=.5)
+    
+    # Plot the fit against the raw data
+    plt.scatter(x_um, power, label='Data')
+    plt.plot(x_um, knifeedge(x_um, *popt), color='r', label=r'Fit: $\frac{1}{e^2}$ radius = ' f'{popt[0]: .0f} $\mu$m')
     plt.xlabel(r'Position ($\mu$m)')
     plt.ylabel(r'Cumulative Power (W)')
     plt.legend()
+    plt.title('Fit of Raw Knife Edge Data')
     plt.show(block=False)
+    
+    # calculate and the spatial profile against the derivative of the fit (i.e. the pdf)
+    
     plt.figure()
-    plt.plot(x_um, norm.pdf(x_um, loc=middle, scale=stddev) / max(norm.pdf(x_um, loc=middle, scale=stddev)))
-    plt.scatter(400, np.exp(-2))
+    dp_dx = FinDiff(0, dx, 1)
+    plt.scatter(x_um, dp_dx(power), marker='o', label='Spatial Profile')
+    plt.plot(x_um, norm.pdf(x_um, loc=popt[1], scale=popt[0] / 2), 'r-', label=r'Fit: $\frac{1}{e^2}$ radius = ' f'{popt[0]: .0f} $\mu$m')
+    plt.legend()
+    plt.title('Fit vs. Calculated Spatial Profile')
+    plt.xlabel(r'Position($\mu$m)')
+    plt.ylabel('Spatial Power (arb.)')
     plt.show()
