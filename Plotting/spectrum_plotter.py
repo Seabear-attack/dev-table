04f483ef7ec5f422f64470712a5962eba713ffb8
Mike Wahl <michael.j.wahl@stonybrook.edu>
2024-02-21 14:38:02 -0500

    Fix .rez files

--- Plotting/spectrum_plotter.py
+++ Plotting/spectrum_plotter.py
@@ -23,27 +23,30 @@ if __name__ == "__main__":
               
     powers_mW = [1 for i in labels]
 
-    n = 3 # the larger n is, the smoother curve will be
+    n = 2 # the larger n is, the smoother curve will be
     b = [1.0 / n] * n
     a = 1
     #processed_data = [np.array([dat[:,0], lfilter(b, a, dat[:,1])]) for dat in raw_data]
     for i, dat in enumerate(raw_data):
-        if np.max(dat[:,1]) < 1000:
-            dat[:,1] = np.pow(2, dat[:,1])
+        if np.max(dat[:,1]) > 1000:
+            dat[:,1] = np.log(dat[:,1])
         np.nan_to_num(dat, copy=False)
-        dat[:,1] = lfilter(b,a,dat[:,1]) 
-        dat[:,1] = dat[:,1] - dat[20,1]
+        dat[:,1] = dat[:,1] / np.max(dat[:,1])
+        #dat[:,1] = lfilter(b,a,dat[:,1]) 
+        #dat[:,1] = dat[:,1] - dat[20,1]
 
     data = [OSAData(dat[:,:2], ('nm', 'mW'), labels[i], powers_mW[i], frep_MHz=60.5) for i, dat in enumerate(raw_data)]
 
     fig, axs = plt.subplots()
 
-    for datum in data:
-        datum.y_axis_units = 'mW'
-        axs.plot(datum._x_axis_data, datum.y_axis_data, label=datum.label)
-    axs.set_xlabel('Wavelength (nm)')
-    axs.set_ylabel(f'Spectral Power (arb.)')
-    axs.legend()
+#    for datum in data:
+#        datum.y_axis_units = 'mW'
+#        axs.plot(datum._x_axis_data, datum.y_axis_data, label=datum.label)
+    for i, datum in enumerate(raw_data):
+        axs.plot(datum[:,0],datum[:,1], label=labels[i])
+        axs.set_xlabel('Wavelength (nm)')
+        axs.set_ylabel(f'Spectral Power (arb.)')
+        axs.legend()
 
     plt.show()
     plt.tight_layout()
